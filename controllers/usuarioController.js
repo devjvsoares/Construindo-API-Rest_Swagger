f139c93d2958f48e473d580a469aa11b0b380a4c
João Vitor <joaosores.jv24@gmail.com>
2025-02-05 21:54:42 -0300

    Comentando status das responses

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -2,8 +2,8 @@ export default class UsuarioController{
     
     listar(req,res){
         let lista = [{nome: "João Vitor", idade: "20"}];
-        //res.status(200).json({ lista });
-        res.status(500).json({msg: "Erro!"});
+        //res.status(200).json({ lista }); //requisição 200 = para retornar acertos/tudo certo
+        res.status(500).json({msg: "Erro!"}); //requisição 500 = para retornar erros
     }
 
     obter(req,res){
